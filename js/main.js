c73fec65af4486c3357ef4b0b68d05c68407b5c6
Whatever1968 <rodneytrevino@yahoo.com>
2016-09-11 00:48:31 -0700

    Teen Quiz App

---
+++ js/main.js
@@ -0,0 +1,146 @@
+// Create the functions
+function one() {
+	document.getElementById("qOne").innerHTML = "Incorrect"
+	document.getElementById("qOne").style.color = "red";
+
+
+}
+function two() {
+	document.getElementById("qOne").innerHTML = "Correct"
+	document.getElementById("qOne").style.color = "green";
+
+}
+
+function three() {
+	document.getElementById("qOne").innerHTML = "Incorrect"
+	document.getElementById("qOne").style.color = "red";
+}
+
+
+
+function four() {
+	document.getElementById("qTwo").innerHTML = "Correct"
+	document.getElementById("qTwo").style.color = "green";
+	
+
+}
+function five() {
+	document.getElementById("qTwo").innerHTML = "Incorrect"
+	document.getElementById("qTwo").style.color = "red";
+	
+
+}
+function six() {
+	document.getElementById("qTwo").innerHTML = "Incorrect"
+	document.getElementById("qTwo").style.color = "red";	
+
+}
+
+function seven() {
+	document.getElementById("qThree").innerHTML = "Incorrect"
+	document.getElementById("qThree").style.color = "red";
+	
+
+}
+function eight() {
+	document.getElementById("qThree").innerHTML = "Incorrect"
+	document.getElementById("qThree").style.color = "red";
+	
+
+}
+function nine() {
+	document.getElementById("qThree").innerHTML = "Correct"
+	document.getElementById("qThree").style.color = "green";	
+
+}
+
+function ten() {
+	document.getElementById("qFour").innerHTML = "Incorrect"
+	document.getElementById("qFour").style.color = "red";
+	
+
+}
+function eleven() {
+	document.getElementById("qFour").innerHTML = "Incorrect"
+	document.getElementById("qFour").style.color = "red";
+	
+
+}
+function twelve() {
+	document.getElementById("qFour").innerHTML = "Correct"
+	document.getElementById("qFour").style.color = "green";	
+
+}
+
+function thirteen() {
+	document.getElementById("qFive").innerHTML = "Incorrect"
+	document.getElementById("qFive").style.color = "red";
+	
+
+}
+function fourteen() {
+	document.getElementById("qFive").innerHTML = "Incorrect"
+	document.getElementById("qFive").style.color = "red";
+	
+
+}
+function fifteen() {
+	document.getElementById("qFive").innerHTML = "Correct"
+	document.getElementById("qFive").style.color = "green";	
+
+}
+
+	
+function sixteen() {
+	document.getElementById("qSix").innerHTML = "Incorrect"
+	document.getElementById("qSix").style.color = "red";
+	
+
+}
+function seventeen() {
+	document.getElementById("qSix").innerHTML = "Incorrect"
+	document.getElementById("qSix").style.color = "red";
+	
+
+}
+function eighteen() {
+	document.getElementById("qSix").innerHTML = "Correct"
+	document.getElementById("qSix").style.color = "green";	
+
+}
+
+function nineteen() {
+	document.getElementById("qSeven").innerHTML = "Incorrect"
+	document.getElementById("qSeven").style.color = "red";
+	
+
+}
+function twenty() {
+	document.getElementById("qSeven").innerHTML = "Incorrect"
+	document.getElementById("qSeven").style.color = "red";
+	
+
+}
+function twentyone() {
+	document.getElementById("qSeven").innerHTML = "Correct"
+	document.getElementById("qSeven").style.color = "green";	
+
+}
+
+function twentytwo() {
+	document.getElementById("qEight").innerHTML = "Incorrect"
+	document.getElementById("qEight").style.color = "red";
+	
+
+}
+function twentythree() {
+	document.getElementById("qEight").innerHTML = "Correct"
+	document.getElementById("qEight").style.color = "green";
+	
+
+}
+function twentyfour() {
+	document.getElementById("qEight").innerHTML = "Incorrect"
+	document.getElementById("qEight").style.color = "red";	
+
+}
