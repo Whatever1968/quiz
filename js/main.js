f0db05c43fd6bd6691bf14a3ba0e2f47bfc0f593
Whatever1968 <rodneytrevino@yahoo.com>
2016-09-11 23:59:32 -0700

    finished the quiz app

--- js/main.js
+++ js/main.js
@@ -1,146 +1,153 @@
 // Create the functions
 function one() {
+	// When button is clicked say this
 	document.getElementById("qOne").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qOne").style.color = "red";
-
-
 }
 function two() {
+	// When button is clicked say this
 	document.getElementById("qOne").innerHTML = "Correct"
+	// When button is clicked change to this color
 	document.getElementById("qOne").style.color = "green";
-
 }
-
 function three() {
+	// When button is clicked say this
 	document.getElementById("qOne").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qOne").style.color = "red";
 }
-
-
-
 function four() {
+	// When button is clicked say this
 	document.getElementById("qTwo").innerHTML = "Correct"
+	// When button is clicked change to this color
 	document.getElementById("qTwo").style.color = "green";
-	
-
 }
 function five() {
+	// When button is clicked say this
 	document.getElementById("qTwo").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qTwo").style.color = "red";
-	
-
 }
 function six() {
+	// When button is clicked say this
 	document.getElementById("qTwo").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qTwo").style.color = "red";	
-
 }
 
 function seven() {
+	// When button is clicked say this
 	document.getElementById("qThree").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qThree").style.color = "red";
-	
-
 }
 function eight() {
+	// When button is clicked say this
 	document.getElementById("qThree").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qThree").style.color = "red";
-	
-
 }
 function nine() {
+	// When button is clicked say this
 	document.getElementById("qThree").innerHTML = "Correct"
+	// When button is clicked change to this color
 	document.getElementById("qThree").style.color = "green";	
-
 }
-
 function ten() {
+	// When button is clicked say this
 	document.getElementById("qFour").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qFour").style.color = "red";
-	
-
 }
 function eleven() {
+	// When button is clicked say this
 	document.getElementById("qFour").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qFour").style.color = "red";
-	
-
 }
 function twelve() {
+	// When button is clicked say this
 	document.getElementById("qFour").innerHTML = "Correct"
+	// When button is clicked change to this color
 	document.getElementById("qFour").style.color = "green";	
-
 }
-
 function thirteen() {
+	// When button is clicked say this
 	document.getElementById("qFive").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qFive").style.color = "red";
-	
-
 }
 function fourteen() {
+	// When button is clicked say this
 	document.getElementById("qFive").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qFive").style.color = "red";
-	
-
 }
 function fifteen() {
+	// When button is clicked say this
 	document.getElementById("qFive").innerHTML = "Correct"
+	// When button is clicked change to this color
 	document.getElementById("qFive").style.color = "green";	
-
-}
-
-	
+}	
 function sixteen() {
+	// When button is clicked say this
 	document.getElementById("qSix").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qSix").style.color = "red";
-	
-
 }
 function seventeen() {
+	// When button is clicked say this
 	document.getElementById("qSix").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qSix").style.color = "red";
-	
-
 }
 function eighteen() {
+	// When button is clicked say this
 	document.getElementById("qSix").innerHTML = "Correct"
+	// When button is clicked change to this color
 	document.getElementById("qSix").style.color = "green";	
-
 }
-
 function nineteen() {
+	// When button is clicked say this
 	document.getElementById("qSeven").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qSeven").style.color = "red";
-	
-
 }
 function twenty() {
+	// When button is clicked say this
 	document.getElementById("qSeven").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qSeven").style.color = "red";
-	
-
 }
 function twentyone() {
+	// When button is clicked say this
 	document.getElementById("qSeven").innerHTML = "Correct"
+	// When button is clicked change to this color
 	document.getElementById("qSeven").style.color = "green";	
-
 }
-
 function twentytwo() {
+	// When button is clicked say this
 	document.getElementById("qEight").innerHTML = "Incorrect"
+	// When button is clicked change to this color
 	document.getElementById("qEight").style.color = "red";
-	
-
 }
 function twentythree() {
-	document.getElementById("qEight").innerHTML = "Correct"
-	document.getElementById("qEight").style.color = "green";
-	
-
+	// When button is clicked say this
+	document.getElementById("qEight").innerHTML = "Incorrect"
+	// When button is clicked change to this color
+	document.getElementById("qEight").style.color = "red";
 }
 function twentyfour() {
-	document.getElementById("qEight").innerHTML = "Incorrect"
-	document.getElementById("qEight").style.color = "red";	
-
+	// When button is clicked say this
+	document.getElementById("qEight").innerHTML = "Correct"
+	// When button is clicked change message to say
+	document.getElementById("change").innerHTML = "Congratulations You've got the High Score!!!"
+	// when button is clicked change background picture to
+	document.body.style.backgroundImage = "url('img/ya.jpg')";
+	// when button is clicked remove the buttons and content
+	document.getElementById("godzilla").style.display="none";		
 }
+
+
+
